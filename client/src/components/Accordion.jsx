8b747e3a242d8e77e3d65f718390c39488659c54
Rasel Kibria <mdraselkibria11@gmail.com>
2024-06-06 12:08:10 +0600

    ui complete

--- client/src/components/Accordion.jsx
+++ client/src/components/Accordion.jsx
@@ -1,12 +1,17 @@
-import React, { useState } from 'react';
-import { IoIosArrowDown ,IoIosArrowUp } from "react-icons/io";
+import React, { useState } from "react";
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 
 const Accordion = ({ items }) => {
   const [activeIndex, setActiveIndex] = useState(null);
+  const [selectedOption, setSelectedOption] = useState('');
 
   const handleSetIndex = (index) => {
     setActiveIndex(activeIndex === index ? null : index);
   };
+
+  const handleChange =(e)=>{
+  setSelectedOption(e.target.value)
+  }
 
   return (
     <div className="max-w-md">
@@ -18,12 +23,28 @@ const Accordion = ({ items }) => {
           >
             <span className="font-semibold">{item.title}</span>
             <span className="text-xl">
-              {activeIndex === index ? <IoIosArrowUp/> : <IoIosArrowDown/>}
+              {activeIndex === index ? <IoIosArrowUp /> : <IoIosArrowDown />}
             </span>
           </button>
           {activeIndex === index && (
-            <div className="py-2 px-5 bg-gray-100">
-              <p>{item.content}</p>
+            <div className="py-3">
+              {
+                item.content.map((ele)=>{
+                  return(
+                    <label className="inline-flex items-center">
+                    <input
+                      type="radio"
+                      className="form-radio h-4 w-4 text-purple-600 transition duration-150 ease-in-out"
+                      label={ele}
+                      value={ele}
+                      onChange={handleChange}
+                      checked={selectedOption === ele}
+                    />
+                    <span className="ml-2">{ele}</span>
+                  </label>
+                  )
+                })
+              }
             </div>
           )}
         </div>
--- client/src/components/product/FilterSideBar.jsx
+++ client/src/components/product/FilterSideBar.jsx
@@ -1,19 +1,18 @@
-import React from 'react';
-import Accordion from '../Accordion';
+import React from "react";
+import Accordion from "../Accordion";
 
 const FilterSideBar = () => {
-    const items = [
-        {
-          title: 'Category',
-          content: 'Content for item 1. This is some more detailed information.',
-        },
-      ];
-    return (
-        <div>
-
-                <Accordion items={items}/>
-        </div>
-    );
+  const items = [
+    {
+      title: "Category",
+      content: ["Mobile", "Accessories", "Geget"],
+    },
+  ];
+  return (
+    <div>
+      <Accordion items={items} />
+    </div>
+  );
 };
 
 export default FilterSideBar;
